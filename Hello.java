1019104890883ffadacc627680851c072028e129
DarkMetal11 <90450153+DarkMetal11@users.noreply.github.com>
2022-02-25 18:23:57 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
+      // Test Only
       System.out.println("Hello, World");
    }
 }
